0c2f7fe5741f9e79eb44f255a20665903a58c58b
mahesh_shetty <147480689+mkannema@users.noreply.github.com>
2023-10-23 13:00:56 +0530

    Update NewProjectApplication.java

--- NewProject/src/main/java/com/example/Restful/NewProjectApplication.java
+++ NewProject/src/main/java/com/example/Restful/NewProjectApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewProjectApplication {
-
+String abc(){
+	return "hello";
+}
 	public static void main(String[] args) {
 		SpringApplication.run(NewProjectApplication.class, args);
 	}
